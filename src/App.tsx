63d5966fa4ddcb0248c291b2d94d73c7e69c269c
Fahad-Alii8 <mfahadali.sparkingasia@gmail.com>
2025-04-08 22:30:41 +0500

    Settingup layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/routes";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello Dev Network App!
-    </h1>
+    <>
+      <RouterProvider router={router} ></RouterProvider>
+    </>
   )
 }
---
+++ src/pages/developers/Developers.tsx
@@ -0,0 +1,6 @@
+
+export default function Developers() {
+    return (
+        <div>Developers</div>
+    )
+}
